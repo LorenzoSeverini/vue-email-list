ecd53018a59dc575e53a9a1b2b9ffc81f536e247
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-05-17 14:35:12 +0200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// vue JS code
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            BoleanApi: 'https://flynn.boolean.careers/exercises/api/random/mail',
+        }
+    }
+});
